26ecc4680c6e43735e996dccb57815b88e87d779
daeni-dang <de021@naver.com>
2022-06-02 13:45:05 +0900

    Daeun: remove_shadow 리턴값 추가, 매개변수로 이미지 위치 입력

--- Remove_shadow.cpp
+++ Remove_shadow.cpp
@@ -7,36 +7,26 @@
 
 using namespace cv;
 using namespace std;
-void remove_shadow() {
 
-	Mat testImage = imread("resource/testimage/omr1_marking.jpg");
-
+Mat remove_shadow(string imgLocate) {
+	CEdge c; CPixel cp; CFilter cf;	CColor cc;
+	
+	Mat testImage = imread(imgLocate);
 	resize(testImage, testImage, Size(testImage.cols / 4, testImage.rows / 4));
-	CEdge c; CPixel cp; CFilter cf;
-	imshow("image1", testImage);
+
 	Mat splits[3];
 	Mat outputSplits[3];
 	Mat dilatedImage;
 	split(testImage, splits);
+
 	for (int i = 0; i < 3; i++) {
 		dilate(splits[i], outputSplits[i], getStructuringElement(MORPH_ELLIPSE, Size(7, 7)));
 		medianBlur(outputSplits[i], outputSplits[i], 21);
 		absdiff(splits[i], outputSplits[i], outputSplits[i]);
 
 		normalize(outputSplits[i], outputSplits[i], 0, 255, NORM_MINMAX);
-
 	}
-	CColor cc;
 	testImage = cc.GS_compositeRGB(outputSplits);
-	//cvtColor(testImage, testImage, COLOR_BGR2GRAY);
-	//testImage += 50;
-	//imshow("image3", testImage);
 
-	//testImage = cp.GS_threshold(testImage, 50, 255, THRESH_BINARY);
-
-	//testImage = c.GS_canny_edge_Canny(testImage, 150, 230);
-
-	imshow("image4", testImage);
-
-	waitKey();
+	return testImage;
 }
